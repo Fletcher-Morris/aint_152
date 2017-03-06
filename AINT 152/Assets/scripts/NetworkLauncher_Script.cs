2eedd64788e4a07e755d00495adfb7fa7a4afa8e
THEBLACKBEAST\odmoa_000 <odmoandtheboy@gmail.com>
2017-03-06 01:00:51 +0000

    World class now gets it's name from the contents of the world name input field.

--- AINT 152/Assets/scripts/NetworkLauncher_Script.cs
+++ AINT 152/Assets/scripts/NetworkLauncher_Script.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class NetworkLauncher_Script : MonoBehaviour {
 
@@ -21,6 +22,7 @@ public class NetworkLauncher_Script : MonoBehaviour {
 
     public void TestSave()
     {
+        _world.worldName = GameObject.Find("Save Name Field").GetComponent<InputField>().text;
         Ship ship1 = new Ship("Voyager", 500);
         Ship ship2 = new Ship("Normandy", 200);
         Ship ship3 = new Ship("Home 1", 1000);
