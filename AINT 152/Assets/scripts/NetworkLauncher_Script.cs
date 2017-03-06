13a0cacd7312c1a6fc3c84c17160cd01e8426655
THEBLACKBEAST\odmoa_000 <odmoandtheboy@gmail.com>
2017-03-06 18:35:27 +0000

    Added World Loader script. World Loader script loads a World instance, and loops through instantiating ships.

--- AINT 152/Assets/scripts/NetworkLauncher_Script.cs
+++ AINT 152/Assets/scripts/NetworkLauncher_Script.cs
@@ -24,8 +24,10 @@ public class NetworkLauncher_Script : MonoBehaviour {
     {
         _world.worldName = GameObject.Find("Save Name Field").GetComponent<InputField>().text;
         Ship ship1 = new Ship("Voyager", 500);
+        ship1.shipPos = new Vector3(-3,0,0);
         Ship ship2 = new Ship("Normandy", 200);
         Ship ship3 = new Ship("Home 1", 1000);
+        ship1.shipPos = new Vector3(3, 0, 0);
         _world.ships.Add(ship1);
         _world.ships.Add(ship2);
         _world.ships.Add(ship3);
---
+++ AINT 152/Assets/scripts/WorldLoader_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldLoader_Script : MonoBehaviour {
+
+    public World theWorld;
+
+    public GameObject shipPrefab;
+
+    void Start()
+    {
+        theWorld.worldName = "New World";
+        theWorld = theWorld.LoadWorld();
+
+        GenerateWorld();
+    }
+
+    public void GenerateWorld()
+    {
+        foreach(Ship _ship in theWorld.ships)
+        {
+            GameObject.Instantiate(shipPrefab, _ship.shipPos, Quaternion.Euler(_ship.shipRot));
+        }
+    }
+}
