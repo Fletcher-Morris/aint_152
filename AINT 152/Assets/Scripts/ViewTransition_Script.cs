332f8637e9f893a1c43915351d127589e5c18122
THEBLACKBEAST\odmoa_000 <odmoandtheboy@gmail.com>
2017-03-13 02:15:43 +0000

    Created ViewTransitionScript. Game spaceship rigidbody more weight.

---
+++ AINT 152/Assets/Scripts/ViewTransition_Script.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewTransition_Script : MonoBehaviour {
+
+    public float crewCamSize = 1.25f;
+    public float shipCamSize = 5;
+
+    public float transitionSpeed = 20;
+
+    public GameObject cameraObject;
+
+    bool isViewingCrew = false;
+    bool isViewingShip = false;
+    public bool isSwitchingToCrew = false;
+    public bool isSwitchingToShip = false;
+
+    public LayerMask crewLayers;
+    public LayerMask shipLayers;
+
+    public void SwitchView()
+    {
+        if(isViewingCrew)
+        {
+            SwitchToShip();
+        }
+        else if(isViewingShip)
+        {
+            SwitchToCrew();
+        }
+        else
+        {
+            SwitchToCrew();
+        }
+    }
+
+    public void SwitchToCrew()
+    {
+        isSwitchingToCrew = true;
+        isSwitchingToShip = false;
+    }
+
+    public void SwitchToShip()
+    {
+        isSwitchingToCrew = false;
+        isSwitchingToShip = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            SwitchView();
+        }
+
+        if (isSwitchingToShip)
+        {
+            cameraObject.GetComponent<Camera>().cullingMask = shipLayers;
+            if (cameraObject.GetComponent<Camera>().orthographicSize < shipCamSize)
+            {
+                cameraObject.GetComponent<Camera>().orthographicSize += transitionSpeed * Time.deltaTime;
+            }
+            else
+            {
+                cameraObject.GetComponent<Camera>().orthographicSize = shipCamSize;
+                isViewingShip = true;
+                isViewingCrew = false;
+                isSwitchingToShip = false;
+                isSwitchingToCrew = false;
+            }
+        }
+        else if (isSwitchingToCrew)
+        {
+            if (cameraObject.GetComponent<Camera>().orthographicSize > crewCamSize)
+            {
+                cameraObject.GetComponent<Camera>().orthographicSize -= transitionSpeed * Time.deltaTime;
+            }
+            else
+            {
+                cameraObject.GetComponent<Camera>().orthographicSize = crewCamSize;
+                cameraObject.GetComponent<Camera>().cullingMask = crewLayers;
+                isViewingShip = false;
+                isViewingCrew = true;
+                isSwitchingToShip = false;
+                isSwitchingToCrew = false;
+            }
+        }
+    }
+}
