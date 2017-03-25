505acd655abbc2696504fc4591d8bda28ea6dd5d
THEBLACKBEAST\odmoa_000 <odmoandtheboy@gmail.com>
2017-03-25 17:27:12 +0000

    Added shooting. Fixed font sizes on world loader ui. Added bullet sprite. Added bullet prefab.

---
+++ Assets/Scripts/Shooting/ShootWeapon_Script.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ShootWeapon_Script : NetworkBehaviour
+{
+    public GameObject bulletSpawnPoint;
+    public GameObject bulletPrefab;
+
+    public float bulletSpeed = 10f;
+    public float destroyTime = 2f;
+
+    private void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            CmdShoot();
+        }
+    }
+
+    [Command]
+    void CmdShoot()
+    {
+        GameObject _bullet = Instantiate(bulletPrefab, bulletSpawnPoint.transform.position, bulletSpawnPoint.transform.rotation);
+
+        _bullet.GetComponent<Rigidbody2D>().velocity = _bullet.transform.up * bulletSpeed;
+
+        NetworkServer.Spawn(_bullet);
+
+        Destroy(_bullet, destroyTime);
+    }
+}
