3d3937902feb51658be7bc9bfd71f6ce9798c56b
THEBLACKBEAST\odmoa_000 <odmoandtheboy@gmail.com>
2017-03-26 00:55:23 +0000

    Created ShipAi script.

---
+++ Assets/Scripts/ShipAi_Script.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipAi_Script : MonoBehaviour
+{
+    public float playerDetectionRange = 100f;
+
+    public GameObject targetPlayerObject;
+}
