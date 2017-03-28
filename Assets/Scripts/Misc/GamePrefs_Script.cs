b9c7401ed811ad21d9447c3c3a7ef543a18c5012
fmorris <fmorris>
2017-03-28 14:04:05 +0100

    Fullscreen toggle in preferences now works.

--- Assets/Scripts/Misc/GamePrefs_Script.cs
+++ Assets/Scripts/Misc/GamePrefs_Script.cs
@@ -21,6 +21,7 @@ public class GamePrefs_Script : MonoBehaviour
         GameObject.Find("Player Name Field").GetComponent<InputField>().text = gamePrefs.playerName;
         GameObject.Find("Player Description Field").GetComponent<InputField>().text = gamePrefs.playerDescrpition;
         GameObject.Find("Volume Slider").GetComponent<Slider>().value = gamePrefs.volumeLevel;
+        GameObject.Find("Fullscreen Toggle").GetComponent<Toggle>().isOn = gamePrefs.fullscreen;
     }
 
     public void GetPrefsFromUI()
@@ -29,6 +30,7 @@ public class GamePrefs_Script : MonoBehaviour
         gamePrefs.playerName = GameObject.Find("Player Name Field").GetComponent<InputField>().text;
         gamePrefs.playerDescrpition = GameObject.Find("Player Description Field").GetComponent<InputField>().text;
         gamePrefs.volumeLevel = GameObject.Find("Volume Slider").GetComponent<Slider>().value;
+        gamePrefs.fullscreen = GameObject.Find("Fullscreen Toggle").GetComponent<Toggle>().isOn;
     }
 
     public void CancelChanges()
@@ -41,6 +43,18 @@ public class GamePrefs_Script : MonoBehaviour
     {
         getPrefsFromUi = false;
         gamePrefs.SavePrefs();
+    }
+
+    public void ApplyChangesLive()
+    {
+        if (gamePrefs.fullscreen)
+        {
+            Screen.SetResolution(Screen.resolutions[Screen.resolutions.Length - 1].width, Screen.resolutions[Screen.resolutions.Length - 1].height, true);
+        }
+        else
+        {
+            Screen.SetResolution(Screen.resolutions[Screen.resolutions.Length - 1].width / 2, Screen.resolutions[Screen.resolutions.Length - 1].height / 2, false);
+        }
     }
 
     void Update()
@@ -57,5 +71,7 @@ public class GamePrefs_Script : MonoBehaviour
         {
             GetPrefsFromUI(); 
         }
+
+        ApplyChangesLive();
     }
 }
